585537958a355a8b158a8fde6fa5c2ec0d91526c
Aed0-623P <aedo.felipe95@gmail.com>
2024-04-16 20:26:08 -0400

    hito 3 punto 3 wip

--- web/models.py
+++ web/models.py
@@ -9,7 +9,7 @@ class Flan(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
     image_url = models.URLField()
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     is_private = models.BooleanField(default=False)
 
     def save(self,*args,**kwargs):
@@ -19,3 +19,12 @@ class Flan(models.Model):
 
     def __str__(self):
         return self.name
+    
+class ContactForm(models.Model):
+    contact_form_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    customer_email = models.EmailField()
+    customer_name = models.CharField(max_length=64)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.customer_name
--- web/views.py
+++ web/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
+from .models import Flan
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    flanes_publicos = Flan.objects.filter(is_private = False)
+    return render(request, 'index.html', {'flanes': flanes_publicos})
 
 def about(request):
     return render(request, 'about.html')
 
 def welcome(request):
-    return render(request, 'welcome.html')
+    flanes_privados = Flan.objects.filter(is_private=True)
+    return render(request, 'welcome.html', {'flanes' : flanes_privados})
 
 def test(request):
     return render(request, 'test.html')
